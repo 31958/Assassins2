fa064ffbbe8dbca8dbcbcdbf2cb5ec39d614face
henrycaushi <zomg.py@gmail.com>
2020-01-19 01:22:54 +0900

    Switch navigation bar example

---
+++ app/src/main/java/com/example/assassins/backend/Player.java
@@ -0,0 +1,37 @@
+package com.example.assassins.backend;
+
+class Player {
+
+    private String name;
+    private boolean alive;
+    private Player target;
+
+    public Player(String s) {
+        this.name = name;
+    }
+
+    public void setAlive(boolean p) {
+        this.alive = p;
+    }
+
+    public boolean getAlive() {
+        return alive;
+    }
+
+    public void setTarget(Player p) {
+        this.target = p;
+    }
+
+    public Player getTarget(Player p) {
+        return target;
+    }
+
+    public void setName(String s) {
+        name = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
