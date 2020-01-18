0dc96187c4017a0e7374647f84d902def57bd8af
Henry Caushi <zomg.py@gmail.com>
2020-01-18 16:31:32 +0000

    Add files via upload

---
+++ app/src/main/java/com/example/assassins/backend/Kill.java
@@ -0,0 +1,34 @@
+package com.example.assassins.backend;
+import java.time.LocalDateTime;
+
+public class Kill {
+    private Player assassin;
+    private Player victim;
+    private Loc location;
+    private LocalDateTime datetime;
+
+    public Kill(Player p1, Loc location, LocalDateTime dt) {
+        this.assassin = null;
+        this.victim = p1;
+        this.location = location;
+        this.datetime = dt;
+    }
+
+    public Kill(Player p1, Player p2, Loc location, LocalDateTime dt) {
+        this.assassin = p1;
+        this.victim = p2;
+        this.location = location;
+        this.datetime = dt;
+    }
+
+    public void updateKill(Player p1) {
+        if (this.assassin.equals(null)) {
+            this.assassin = p1;
+        }
+
+        else {
+            throw new UnsupportedOperationException("Kill already has a confirmed killer");
+        }
+    }
+
+}
